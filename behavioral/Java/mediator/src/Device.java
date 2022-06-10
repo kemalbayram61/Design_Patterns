588d6b28600e0f182c8750fa4bc176e9bb7a652a
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-06-10 12:09:49 +0300

    java mediator design pattern device class write initial

--- behavioral/Java/mediator/src/Device.java
+++ behavioral/Java/mediator/src/Device.java
@@ -1,2 +1,13 @@
-public interface Device {
+public abstract class Device {
+
+    //TODO: mediator type update
+    protected String mediator;
+
+    public Device(String mediator){
+        this.mediator = mediator;
+    }
+
+    public abstract void runTest();
+
+    public abstract void endTest();
 }
